b06d85197c511e1b82d5b92979839869ae0aaed4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-23 16:59:25 +0000

    Deploy fahro/synonyms to github.com/fahro/synonyms.git:gh-pages

---
+++ precache-manifest.bc8888edcab3d35a5953a5e300b380c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2b62a3d3c9142152429e599e50fdee",
+    "url": "/synonyms/index.html"
+  },
+  {
+    "revision": "01f5e37e89c876f08825",
+    "url": "/synonyms/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "284bd4f13f116d1102bb",
+    "url": "/synonyms/static/css/main.5b7592c7.chunk.css"
+  },
+  {
+    "revision": "01f5e37e89c876f08825",
+    "url": "/synonyms/static/js/2.65152f2a.chunk.js"
+  },
+  {
+    "revision": "284bd4f13f116d1102bb",
+    "url": "/synonyms/static/js/main.77b21cc8.chunk.js"
+  },
+  {
+    "revision": "079a01e67d11b6bad62a",
+    "url": "/synonyms/static/js/runtime-main.82dfeb91.js"
+  }
+]);
