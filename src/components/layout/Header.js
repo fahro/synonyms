88908db74b7499426437a8a28609154769f7a595
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-12-23 17:57:14 +0100

    Initial commit with images bug

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import { Navbar } from 'reactstrap';
-import { Link } from 'react-router-dom';
 
 class Header extends Component {
 	render() {
 		return (
-			<Navbar color={'dark'} dark>
-				<Link to="/">Synoyms Dictionary App</Link>
-			</Navbar>
+			<div className="col-md-12">
+				<div className="aa-header-area">
+					<div className="row">
+					<div className="col-md-6 col-sm-6 col-xs-6">
+						<div className="aa-header-left">
+						<div className="aa-telephone-no">
+							<span className="fa fa-phone"></span>
+							1-900-523-3564
+						</div>
+						<div className="aa-email hidden-xs">
+							<span className="fa fa-envelope-o"></span> info@markups.com
+						</div>
+						</div>              
+					</div>
+					<div className="col-md-6 col-sm-6 col-xs-6">
+						<div className="aa-header-right">
+						<a href="register.html" className="aa-register">Register</a>
+						<a href="signin.html" className="aa-login">Login</a>
+						</div>
+					</div>
+					</div>
+				</div>
+        	</div>
 		);
 	}
 }
